2c534ad24dbea63c79aab5b8d9e732be94ed2d1e
godovikoshka <godovikoshka@gmail.com>
2021-06-03 22:30:34 +0300

    Добавьте файлы проекта.

---
+++ OOP4/Lab4.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+
+namespace OOP4
+{
+    public class Lab4
+    {
+        static Mutex _mutex;
+
+        public static bool IsSingleInstance()
+        {
+            try
+            {
+                Mutex.OpenExisting("mut");
+            }
+            catch
+            {
+                _mutex = new Mutex(true, "mut");
+                return true;
+            }
+
+            return false;
+        }
+
+        public static int Main()
+        {
+            if (!IsSingleInstance())
+            {
+                Console.WriteLine("More than one instance");
+                return -1;
+            }
+            else
+            {
+                Console.WriteLine("One instance");
+                return 0;
+            }
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using OOP4;
+using System.Threading;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int k = 1;
+            var t1 = new Thread(() => k = Lab4.Main());
+            t1.Name = "Thread 1";
+            var t2 = new Thread(() => k = Lab4.Main());
+            t2.Name = "Thread 2";
+            var t3 = new Thread(() => k = Lab4.Main());
+            t3.Name = "Thread 3";
+            t1.Start();
+            t2.Start();
+            t3.Start();
+            t1.Join();
+            t2.Join();
+            t3.Join();
+
+            Assert.AreEqual(0, k);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int k = 1;
+            var t1 = new Thread(() => k = Lab4.Main());
+            t1.Name = "Thread 1";
+            t1.Start();
+            t1.Join();
+
+            Assert.AreEqual(-1, k);
+        }
+    }
+}
